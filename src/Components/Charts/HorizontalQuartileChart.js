e77f60cb18efc0d57064286f2d371900875d77fd
Mark Kauffman <markkauffman22@gmail.com>
2020-03-03 23:28:41 -0700

    svg height-width are props. scale rockin', Axis fix

--- src/Components/Charts/HorizontalQuartileChart.js
+++ src/Components/Charts/HorizontalQuartileChart.js
@@ -4,17 +4,26 @@ import Chart from "./Chart";
 
 
 class HorizontalQuartileChart extends Chart {
-
     addChartLayer({svg, values}) {
+        const{ svg_width, svg_height} = this.props;
 
         let {q1, median, q3, max, userScore} = values;
 
         // Setup svg using d3's margin convention
 
-        var margin = {top: 40, right: 130, bottom: 5, left: 30};
+        var margin = {top: 25, right: 130, bottom: 5, left: 30};
 
-        var height = 200 - margin.top - margin.bottom,
-            width = 640 - margin.left - margin.right;
+
+        var height = svg_height - margin.top - margin.bottom,
+            width = svg_width - margin.left - margin.right;
+
+        // Set tick value based on chart width ...
+        var numTicks = 1;
+        if(width < 100 ) numTicks = 1
+            else if (width >= 100 && width < 200 ) numTicks = 3
+            else if (width >= 200 && width < 300 ) numTicks = 5
+            else if (width >= 300 && width < 480 ) numTicks = 9
+            else numTicks = 13 ;      
 
         svg
             .attr("width", width + margin.left + margin.right)
@@ -53,17 +62,19 @@ class HorizontalQuartileChart extends Chart {
             .range([0, height])
             .padding([0.2])
         svg.append("g")
-            .attr("transform", "translate(0,20)")
+            .attr("transform", "translate(0, 20)")
 
         // Add X axis
+        var axis_top = margin.top -     20;
         var x = d3.scaleLinear()
-            // .domain([0, 1400])
             .domain([0, max])
             .range([ 0, width ]);
         svg.append("g")
-            .attr("transform", "translate(0, -20)")
-            .call(d3.axisBottom(x));
+            // .attr("transform", "translate(0, -20)")
+            .attr("transform", "translate(" + margin.left + ", " + axis_top + ")")
 
+            .call(d3.axisBottom(x)
+                    .ticks(numTicks));
 
 // Create a group, g, element for each series
         var g = svg.select('g')
@@ -81,13 +92,6 @@ class HorizontalQuartileChart extends Chart {
             .data(function(d) {
                 return d;
             })
-
-            // .enter()
-            //   .append('rect')
-            //   .attr('width', function(d) {return d[1] - d[0]; })
-            //   .attr('x', function(d) { return d[0]; })
-            //   .attr('y', function(d, i) { return i * 20; })
-            //   .attr('height', 19);
 
             .enter()
             .append("rect")
@@ -114,7 +118,7 @@ class HorizontalQuartileChart extends Chart {
             .attr("opacity", 0)
             .attr("cy", (y.bandwidth()*.25))    // location in bar
             .attr("cx", 0)   // Starting cond. for X
-            .attr("r", (y.bandwidth()*.185))
+            .attr("r", (y.bandwidth()*.20))
             .attr("fill", "#515252")
             .style("stroke", "black")
             .style("stroke-width", 1.5)
@@ -125,21 +129,29 @@ class HorizontalQuartileChart extends Chart {
 
         g.append("text")
             .text(scoreVal)
-            // .attr("font-size", 12)
             .attr("fill", "black")
             .attr("opacity", 0)
-            .attr("y",(y.bandwidth() * .6 ))
+            .attr("y",(y.bandwidth() * .65 ))
             .attr("x", scoreScale(scoreVal) - 10)
             .transition(t)
-            .attr("opacity", 1)
+                .attr("opacity", 1)
 
+        g.append("text")        
+            .text("Your Score")
+            .attr("fill", "black")
+            .attr("opacity", 0)
+            .attr("y",(y.bandwidth() * .8 ))
+            .attr("x", scoreScale(scoreVal) - 25)
+            .transition(t)
+                .attr("opacity", 1)              
 
 // Draw legend
+        var drop = 25;
         var legend = svg.selectAll(".legend")
             .data(colors)
             .enter().append("g")
             .attr("class", "legend")
-            .attr("transform", function(d, i) { return "translate(50," + i * 19 + ")"; });
+            .attr("transform", function(d, i) { return "translate(70," + (drop +(i * 19)) + ")"; });
 
         legend.append("rect")
             .attr("x", width - 18)
---
+++ src/Components/Token.md
@@ -0,0 +1,4 @@
+// Token:
+// ?token=coachdemo:13775053e3f0176b60aa2390069820bf33efcae9:BB
+
+
