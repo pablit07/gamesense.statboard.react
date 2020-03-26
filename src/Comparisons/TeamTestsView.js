1250626268a347a0cf873e632372790398647ff0
Paul Kohlhoff <paul.kohlhoff@gmail.com>
2020-03-25 22:12:27 -0600

    working radio buttons

--- src/Comparisons/TeamTestsView.js
+++ src/Comparisons/TeamTestsView.js
@@ -2,22 +2,27 @@ import React, {Component, Fragment} from 'react';
 
 import TeamTestsPrScoreContainer from "./TeamTestsPrScoreContainer";
 import TeamCompareChart from "../Components/Charts/TeamCompareChart";
-import RadioButton from "../Buttons/RadioButton";
+import RadioButtons from "../Buttons/RadioButtons";
 
 class TeamTestsView extends Component {
 
     render() {
 
         let style = {marginLeft: '3.3rem'};
+
+        function handleSelect(v) {
+            alert('it worked: ' + v)
+        };
+
         return (<Fragment>
 
             <h4 style={style}>TeamTestView</h4>
 
             <TeamTestsPrScoreContainer socket={this.props.socket}>
 
-              <RadioButton />
+              <RadioButtons options={ [{name:'Type', value:'type'}, {name:'Location', value:'location'}, {name:'Total', value:'total'}] } initSelectedOption={'type'} handleSelect={handleSelect} />
               <br />
-              <TeamCompareChart svg_width={385} svg_height= {160}/>
+              <TeamCompareChart svg_width={385} svg_height={160}/>
           
             </TeamTestsPrScoreContainer>
 
